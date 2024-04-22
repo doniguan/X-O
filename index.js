07a196a8d6a2de0cf4d91fa87daa65d33bab40e2
Sergey Pimonov <doniguan001@gmail.com>
2024-04-22 20:30:21 +0300

    working version

--- index.js
+++ index.js
@@ -1,45 +1,146 @@
-let win = false
-//флаг для проверки выполнения условия победы
+//дописать показ предложения играть снова
+//дописать возможность повторной игры
+
 let firstMoveIsHuman = false
-//проверка очерёдности хода
+let winnerFound = false
 let cells = []
-//клетки, 9 штук, внутри свойств заполнено/незаполнено
+let humanIcon = 'O'
+let computerIcon = 'X'
+let resultMessage
 
 onload = function () {
+  resultMessage = document.getElementById('win_message')
+
   const random = Math.floor(Math.random() * 2) + 1
   if (random === 1) {
     firstMoveIsHuman = true
+    humanIcon = 'X'
+    computerIcon = 'O'
   }
 
-  while (!win) {
-    move()
+  cells = document.getElementsByClassName('cell')
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', function () {
+      humanMove(this)
+    })
+  }
+
+  if (!firstMoveIsHuman) {
+    computerMove()
   }
 }
 
-move = function () {
-  if (firstMoveIsHuman) {
-    humanMove
-  } else {
-    computerMove
+humanMove = function (item) {
+  if (item.hasChildNodes()) {
+    return false
   }
+  item.innerHTML = humanIcon
+  winnerFound = winChk()
 
-  if (winCheck()) {
+  if (!winnerFound) {
+    computerMove()
+    winnerFound = winChk()
+    if (!winnerFound) {
+      if (!checkFreeSpace()) {
+        drawShowing()
+      }
+    } else {
+      winShowing()
+    }
+  } else {
     winShowing()
   }
-
-  firstMoveIsHuman = !firstMoveIsHuman;
 }
 
 computerMove = function () {
-  alert("computer move")
+  while (true) {
+    if (checkFreeSpace()) {
+      const random = Math.floor(Math.random() * 9)
+      if (!cells[random].hasChildNodes()) {
+        cells[random].innerHTML = computerIcon
+        return true
+      }
+    } else {
+      drawShowing()
+      return false
+    }
+  }
 }
 
-humanMove = function () {
-  alert("human move")
+checkFreeSpace = function () {
+  for (let i = 0; i < cells.length; i++) {
+    if (!cells[i].hasChildNodes()) {
+      return true
+    }
+  }
+  return false
 }
 
-winCheck = function () {
-  return win
+winnerChk = function (item) {
+  if (item.innerHTML === humanIcon) {
+    return true
+  } else {
+    return false
+  }
 }
 
-winShowing = function () {}
+winChk = function () {
+  if (
+    ((cells[0].innerHTML === cells[4].innerHTML &&
+      cells[4].innerHTML === cells[8].innerHTML) ||
+      (cells[2].innerHTML === cells[4].innerHTML &&
+        cells[4].innerHTML === cells[6].innerHTML) ||
+      (cells[3].innerHTML === cells[4].innerHTML &&
+        cells[4].innerHTML === cells[5].innerHTML) ||
+      (cells[1].innerHTML === cells[4].innerHTML &&
+        cells[4].innerHTML === cells[7].innerHTML)) &&
+    cells[4].hasChildNodes()
+  ) {
+    winnerIsHuman = winnerChk(cells[4])
+    winnerFound = true
+    return true
+  } else {
+    if (
+      ((cells[0].innerHTML === cells[1].innerHTML &&
+        cells[0].innerHTML === cells[2].innerHTML) ||
+        (cells[0].innerHTML === cells[3].innerHTML &&
+          cells[0].innerHTML === cells[6].innerHTML)) &&
+      cells[0].hasChildNodes()
+    ) {
+      winnerIsHuman = winnerChk(cells[0])
+      winnerFound = true
+      return true
+    } else {
+      if (
+        ((cells[6].innerHTML === cells[7].innerHTML &&
+          cells[8].innerHTML === cells[7].innerHTML) ||
+          (cells[2].innerHTML === cells[5].innerHTML &&
+            cells[5].innerHTML === cells[8].innerHTML)) &&
+        cells[8].hasChildNodes()
+      ) {
+        winnerIsHuman = winnerChk(cells[8])
+        winnerFound = true
+        return true
+      } else return false
+    }
+  }
+}
+
+winShowing = function () {
+  resultMessage.style.display = 'block'
+
+  if (winnerIsHuman) {
+    resultMessage.innerHTML = 'Вы выиграли!'
+    resultMessage.style.color = 'green'
+  } else {
+    resultMessage.innerHTML = 'Вы проиграли.'
+    resultMessage.style.color = 'red'
+  }
+}
+
+drawShowing = function () {
+  resultMessage.innerHTML = 'Ничья :|'
+  resultMessage.style.color = 'blue'
+  resultMessage.style.display = 'block'
+}
